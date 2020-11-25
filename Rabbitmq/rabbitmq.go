aca38350c8b37b5b018bce6be44f2a1217ee1da6
wukaibin <429148925@qq.com>
2020-11-26 00:38:30 +0800

    简单模式下生产代码

--- Rabbitmq/rabbitmq.go
+++ Rabbitmq/rabbitmq.go
@@ -56,3 +56,38 @@ func (r *RabbitMQ) failOnErr(err error, message string)  {
 func newRabbitMQSimple(queueName string) *RabbitMQ {
 	return newRabbitMQ(queueName, "", "")
 }
+
+// 简单模式下生产代码
+func (r *RabbitMQ) PublishSimple (message string) {
+	// 1.申请队列,如果队列不存在会自动创建,如果存在,则跳过创建
+	// 保证队列存在,消息能发送到队列中
+	_ , err := r.channel.QueueDeclare(
+			r.QueueName,
+			// 是否持久化
+			false,
+			// 是否自动删除
+			false,
+			// 是否具有排他性
+			false,
+			// 是否阻塞
+			false,
+			// 额外属性
+			nil,
+		)
+	if err != nil {
+		fmt.Printf(err)
+	}
+
+	// 2. 发送消息到队列中
+	r.channel.Publish(
+		r.Exchange,
+		r.QueueName,
+		// 如果为true,根据exchange类型和routkey规则,如果无法找到符合条件的队列,那么会把发送消息返回给发送者
+		false,
+		// 如果为true,当exchange发送消息到队列后发现队列上没有绑定消费者,则会把消息发还给发送者
+		false,
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(message),
+		})
+}
